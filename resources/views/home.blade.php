f4ae5118dc24584eea0e57a5a27161b4fe038001
sarocha tongtabrat <srocha.tangmo@gmail.com>
2021-08-18 20:12:40 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+<div class="container">
+
+    <div class="row justify-content-center">
+
+        <div class="col-md-8">
+
+            <div class="card">
+
+                <div class="card-header"><h2><strong>{{ __('My Profile') }}</strong></h2></div>
+
+
+
+                <div class="card-body">
+
+                    @if (session('status'))
+
+                        <div class="alert alert-success" role="alert">
+
+                            {{ session('status') }}
+
+                        </div>
+
+                    @endif
+                    <strong>  username : </strong> {{ Auth::user()->username }} <br>
+                    <strong>  Phone : </strong> {{ Auth::user()->phone }} <br>
+
+                    <a href="" class="btn btn-primary">Product Mangements</a>
+                    <a href="" class="btn btn-success">Home</a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
